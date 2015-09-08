073036e4064cf520c2adf8de83cec73bd5c32b62
Gerhard Pegel <gpegel@gmail.com>
2015-09-08 23:35:49 +0200

    adding populated list of countries (not working yet)

--- OpenSSL_UWA/OpenSSL_UWA/MainPage.xaml.cs
+++ OpenSSL_UWA/OpenSSL_UWA/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Globalization;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -20,11 +21,32 @@ namespace OpenSSL_UWA
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
         }
+
+        //        public static List<string> CountryList()
+        //{
+        //    List<string> CultureList = new List<string>();
+        //    CultureInfo[] getCultureInfo = CultureInfo.GetCultures(CultureTypes.SpecificCultures);
+
+        //    foreach (CultureInfo getCulture in getCultureInfo)
+        //        RegionInfo GetRegionInfo = new RegionInfo(getCulture.LCID);
+        //    if (!(CultureList.Contains(GetRegionInfo.EnglishName)))
+        //    {
+        //        CultureList.Add(GetRegionInfo.EnglishName);
+
+        //    }
+        //    CultureList.Sort();
+        //    return CultureList;
+
+        //}
+
     }
-}
+
+    }
+
