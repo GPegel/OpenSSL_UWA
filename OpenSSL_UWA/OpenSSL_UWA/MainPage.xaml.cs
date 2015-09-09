e9ba15e702a5b421523e6b6d9c792dadb5d144ef
Gerhard Pegel <gpegel@gmail.com>
2015-09-09 23:00:58 +0200

    first part of populating the openSSL command to create a CSR is ready.

--- OpenSSL_UWA/OpenSSL_UWA/MainPage.xaml.cs
+++ OpenSSL_UWA/OpenSSL_UWA/MainPage.xaml.cs
@@ -14,20 +14,69 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using System.Globalization;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace OpenSSL_UWA
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
 
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            //  i'm declaring all the variables and objects to use them in a array
+            //  The reason for doing this is because I want the opensslCommandBox getting filled
+            //  automatically when entering the values in the text and combo boxes.
+
+            var commonName = commonNameTextBox.Text;
+            var company = companyNameTextBox.Text;
+            var department = departmentTextBox.Text;
+            var email = eMailAddresTextBox.Text;
+            var city = cityNameTextBox.Text;
+            var stateProvince = stateProvinceTextBox.Text;
+
+            //  The country, keysize and hash vars are not working in this way. I need another 
+            //  solution for this.
+
+            //var country = countryCombo.SelectedItem;
+            //var keySize = keySizeCombo.SelectedItem;
+            //var hash = hashAlgorithmCombo.SelectedItem;
+
+            string[] commands = new string[]
+
+            {
+                "openssl req -new - sha256 - newkey rsa: " +
+                keySize + " " + "- nodes -out " +
+                commonName + ".csr" + " - keyout " + commonName + ".key" +
+                " -subj " + " " +
+                "'/C=" + countryCombo +
+                "/ST=" + stateProvince +
+                "/L=" + city +
+                "/O=" + company +
+                "/OU=" + department +
+                "/CN=" + commonName +
+                "/emailAddress=" + email + ""
+
+            };
+
+            //  This line is generated at OpenProvider and I've tested it and this works. 
+            //  So the code above should do the trick
+            //  openssl req -new - sha256 - newkey rsa: 4096 - nodes -out your.csr - keyout your.private.key -subj "/C=NL/ST=Overijssel/L=Enschede/O=GP Software/OU=IT/CN=www.gerhardpegel.nl/emailAddress=gpegel@gmail.com"
+
+
+            foreach (string command in commands)
+
+            {
+                opensslCommandBox.Text = (command);
+
+            }
+        }
+
+        //  This piece of code I'm going to use later to do a automatic population
+        //  of the countryComboBox from an online service.
 
         //        public static List<string> CountryList()
         //{
@@ -48,5 +97,6 @@ namespace OpenSSL_UWA
 
     }
 
-    }
+}
+
 
